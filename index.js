8e226b7099e4bfdf92af5ea6c5337ab973abced2
Богданов Сергей Иванович <sergey.bogdanov@geropharm.com>
2020-10-14 21:01:34 +0300

    f7

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const returnFile = (url) => {
-    fs.readFile(path.join(__dirname, url), function (err,data) {
+    fs.readFile(`${__dirname}/${url}`, function (err,data) {
         if (err) {
           res.writeHead(404);
           res.end(JSON.stringify(err));
@@ -19,7 +19,7 @@ Server((req, res) => {
         case '/v8': return res.end(`${process.versions.v8}`);
         case '/node': return res.end(`${process.version}`);
         case '/day': return String(new Date().getDate());
-        case '/mirror?x=XYZ': return res.end(req.query.x);
+        case '/mirror': return res.end(req.query.x);
         case '/package.json': return returnFile(req.url);
         default: return res.end('bogdanovsi');
     }
